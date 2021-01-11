edec694b4e3f0df4f441570f10ff12e2f63b8ad0
unknown <lucasngarcia8@hotmail.com>
2021-01-11 13:43:32 -0300

    Projeto criado

---
+++ src/test/java/com/lucasgarcia/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgarcia.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
